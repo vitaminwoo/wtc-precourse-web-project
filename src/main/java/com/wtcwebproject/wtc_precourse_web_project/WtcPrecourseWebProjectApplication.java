fb09b9570a61224d7bbd04d0f887084bc0a7efbd
minwoo_park <nwpark111@gmail.com>
2025-11-12 19:30:47 +0900

    init: Initial wtc web project

---
+++ src/main/java/com/wtcwebproject/wtc_precourse_web_project/WtcPrecourseWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wtcwebproject.wtc_precourse_web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcPrecourseWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtcPrecourseWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtcwebproject/wtc_precourse_web_project/WtcPrecourseWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtcwebproject.wtc_precourse_web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtcPrecourseWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
